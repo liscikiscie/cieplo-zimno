b0260362ca6fd9c64ccbbfae34012dd5250942cf
notFromHere <jakubwasiljew@gmail.com>
2018-09-18 20:21:40 +0200

    about looks fine

---
+++ src/componets/AboutUs.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="aboutUs-container">
+        <div class="info-container">
+            <div class="info publisher">
+                {{publisher}}
+            </div>
+            <div class="info coops">
+                {{coops}}
+            </div>
+            <div class="info invitation">
+                {{invitation}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    let AboutUs = {
+        props: [ 'publisher', 'coops', 'invitation' ],
+        name: 'AboutUs',
+        data() {
+            return {}
+        }
+    };
+    export default AboutUs;
+
+</script>
+
+<style scoped lang="scss">
+    .aboutUs-container {
+        height: 600px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .info {
+            text-align: justify;
+            padding-top: 30px;
+        }
+    }
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,40 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div class="about-container">
+        <AboutUs
+                v-for="information in informations"
+                :key="information.id"
+                :publisher="information.publisher"
+                :coops="information.coops"
+                :invitation="information.invitation">
+        </AboutUs>
     </div>
 </template>
 
 <script>
+    import AboutUsInfo from '../assets/AboutUs';
+    import AboutUs from '../componets/AboutUs.vue';
+
     let About = {
-        name: 'About'
+        name: 'About',
+        components: {
+            AboutUs
+        },
+        data() {
+            return {
+                informations: AboutUsInfo
+            }
+        }
     };
     export default About;
 </script>
 
 <style scoped lang="scss">
+    .about-container {
+        margin: 0 15%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
 
 </style>
